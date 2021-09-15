b9a2d2ea541c89dd935d5f8df2a7b97487cde10f
ThevinduSiriwardana <phtdsiriwardana@students.nsbm.ac.lk>
2021-09-15 21:54:21 +0530

    Adding registration from

---
+++ create.php
@@ -0,0 +1,35 @@
+<?php
+
+$host="sql308.epizy.com";
+$user="epiz_28147165";
+$pass="MiX72XqXwIa8eL";
+$db="epiz_28147165_login_account";
+
+
+$username=$_POST["username"];
+$password=$_POST["password"];
+$address=$_POST["address"];
+$gender=$_POST["Gender"];
+$email=$_POST["email"];
+
+//Insert data
+
+$con= mysqli_connect($host,$user,$pass,$db);
+mysqli_select_db($con,"login_account");
+$sql ="INSERT INTO users VALUES('$username','$password','$address','$gender','$email')";
+$ret= mysqli_query($con,$sql);
+mysqli_close($con);
+if (mysqli_num_rows($result)==1)
+{
+	echo("Welcome! <a href='home.html'></a>");
+	header("location: home.html");
+	exit;
+
+}
+else
+{
+	echo("Something went wrong");
+}
+}
+
+exit;
